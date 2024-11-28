f8145dce7f15f8f363cbe7402be58d47db89b9a9
BEN LAAZIZ HAMZA <benlaaziz.hamza@gmail.com>
2024-11-28 18:25:25 +0100

    cpp03 ex00

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,46 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 17:33:49 by hben-laz          #+#    #+#             */
-/*   Updated: 2024/11/28 17:33:50 by hben-laz         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:23:00 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+class Dog{//base class
+
+    public :
+        Dog(){}
+            
+        void Brak(){
+            std::cout<<"Brak!"<<std::endl;
+        }
+        
+        void Walk()
+        {
+            x+= 1;
+            y+= 1;
+        }
+
+        float x,y;
+        
+};
+
+
+
+
+class Golden : public Dog{
+    
+    
+};
+
+int main()
+{
+
+    Golden dog1;
+    dog1.Brak();
+
+
+    return 0;
+    
+}
