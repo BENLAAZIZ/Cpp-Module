01ce5c6e194311c50c51faa35a1acefb11fd0433
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2024-12-26 23:18:02 +0100

    cpp01

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,25 +6,33 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 00:20:41 by hben-laz          #+#    #+#             */
-/*   Updated: 2024/12/24 23:09:24 by hben-laz         ###   ########.fr       */
+/*   Updated: 2024/12/26 23:00:46 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main()
+int main(int ac, char **argv)
 {
 
     Harl harl;
     std::string str;
-    std::cout<<"enter one of : < DEBUG, INFO, WARNING, ERROR > :";
+    // std::cout<<"enter one of : < DEBUG, INFO, WARNING, ERROR > :";
     
-    std::getline(std::cin, str);
-    if (std::cin.eof())
-        return 1;
-    if (str != "DEBUG" && str != "INFO" && str != "WARNING" && str != "ERROR")
-        std::cout<<"invalid input !!"<<std::endl;
-    else
+    // std::getline(std::cin, str);
+    // if (std::cin.eof())
+    //     return 1;
+    // if (str != "DEBUG" && str != "INFO" && str != "WARNING" && str != "ERROR")
+    //     std::cout<<"invalid input !!"<<std::endl;
+    // else
+    if (ac == 2)
+    {
+        str = argv[1];
         harl.complain(str);
+    }
+    else
+    {
+        std::cout<<"invalid input !!"<<std::endl;
+    }
     return 0;
 }   
