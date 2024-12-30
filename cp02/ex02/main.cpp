5f5eb566e00e301009f902c768bd86b8735db240
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2024-12-30 18:13:18 +0100

    cpp02

--- cp02/ex02/main.cpp
+++ cp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 22:14:31 by hben-laz          #+#    #+#             */
-/*   Updated: 2024/12/21 23:51:40 by hben-laz         ###   ########.fr       */
+/*   Updated: 2024/12/30 18:13:05 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,10 @@ int main( void )
     std::cout << a << std::endl;
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
+    
     std::cout << b << std::endl;
+    
     std::cout << Fixed::max( a, b ) << std::endl;
-
-    bool t;
-    Fixed c(1);
-    Fixed d(2);
-    Fixed e(3);
-    Fixed f;
-    f = c - 1;
-    ++c;
-    std::cout << c << std::endl;
 
     return 0;
 }
