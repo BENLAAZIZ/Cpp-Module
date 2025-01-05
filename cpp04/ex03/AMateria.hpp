238e102bd5bd37c7eece0c5b60dede777f61463e
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-01-05 02:50:32 +0100

    ex03

--- cpp04/ex03/AMateria.hpp
+++ cpp04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 23:09:07 by hben-laz          #+#    #+#             */
-/*   Updated: 2025/01/05 01:57:28 by hben-laz         ###   ########.fr       */
+/*   Updated: 2025/01/05 02:30:36 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@
 
 class AMateria
 {
-    protected:
-        std::string type;
-    public:
-        AMateria();
-        AMateria(std::string const& type);
-        AMateria( AMateria const& src );
-        virtual ~AMateria();
-        AMateria& operator=( AMateria const& rhs );
-        std::string const& getType() const;
-        virtual AMateria* clone() const = 0;
-        virtual void use(ICharacter& target);
+	protected:
+		std::string type;
+	public:
+		AMateria();
+		AMateria(std::string const& type);
+		AMateria( AMateria const& src );
+		virtual ~AMateria();
+		AMateria& operator=( AMateria const& rhs );
+		std::string const& getType() const;
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
 };
 
 #endif
