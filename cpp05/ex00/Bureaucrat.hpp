e26ce92f9354e3de0b0c7696820618803b5f6aa3
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-01-17 20:07:43 +0100

    cpp 05

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 04:03:49 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/01/17 17:52:01 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class  Bureaucrat {
+    private:
+        const std::string name;
+        int grad;
+    public:
+        Bureaucrat(const std::string _name, int _grad);
+        Bureaucrat(const Bureaucrat& obj);
+        Bureaucrat& operator=(const Bureaucrat& obj);
+        ~Bureaucrat();
+
+        std::string getName() const;
+        int getGrade() const;
+
+        void increment();
+        void decrement();
+        
+        class GradeTooHighException : public std::exception {
+            const char* what() const throw(); 
+        };
+        
+        class GradeTooLowException : public std::exception {
+            const char* what() const throw();
+        };
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj);
+
+
+#endif
