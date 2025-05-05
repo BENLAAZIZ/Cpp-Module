7ef484d2bd722f83f3bb63b2c5b31f7529cb7283
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-05 14:47:49 +0100

    ex00

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 12:47:23 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/05/05 13:21:57 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <climits>
+#include <cfloat>
+#include <cmath>
+#include <sstream>
+
+class ScalarConverter {
+    private:
+        ScalarConverter(); // Must be private to prevent instantiation
+        ScalarConverter(const ScalarConverter& other); // Copy constructor
+        ScalarConverter& operator=(const ScalarConverter& other); // Assignment operator
+        ~ScalarConverter(); // Destructor
+    public:
+        static void convert(const std::string& literal);
+};
+
+#endif
