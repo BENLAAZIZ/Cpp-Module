f3418a569d8f917a0119f1b7c4c007848f12948e
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-06 19:20:31 +0100

    cpp07 ex00 and ex01

---
+++ cpp07/ex00/ whatever.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    whatever.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 18:53:22 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/05/06 19:05:12 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+template <typename T>
+
+T max(T a, T b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+
+#endif
---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 19:11:49 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/05/06 19:19:35 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+
+template <typename T>
+void print(T& element)
+{
+    std::cout << element << " ";
+}
+
+template <typename T>
+void iter(T* array, int len, void (*func)(T&))
+{
+    for (int i = 0; i < len; ++i)
+    {
+        func(array[i]);
+    }
+    std::cout << std::endl;
+}
+
+#endif
