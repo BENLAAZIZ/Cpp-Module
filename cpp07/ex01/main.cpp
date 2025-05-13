2417e1be0cbb66ca41a093d71c4f04930562cf28
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-13 22:50:03 +0100

    cpp07 update

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 19:12:13 by hben-laz          #+#    #+#             */
-/*   Updated: 2025/05/13 22:45:38 by hben-laz         ###   ########.fr       */
+/*   Updated: 2025/05/13 22:49:33 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,36 +34,4 @@ int main()
 
     return 0;
 }
-
-// class Awesome
-// {
-//   public:
-//     Awesome( void ) : _n( 42 ) { return; }
-//     int get( void ) const { return this->_n; }
-//   private:
-//     int _n;
-// };
-
-// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-// {
-//   o << rhs.get();
-//   return o;
-// }
-
-// template< typename T >
-// void print( T const & x )
-// {
-//   std::cout << x << std::endl;
-//   return;
-// }
-
-// int main() {
-//   int tab[] = { 0, 1, 2, 3, 4 };
-//   Awesome tab2[5];
-
-//   iter( tab, 5, print<const int> );
-//   iter( tab2, 5, print<Awesome> );
-
-//   return 0;
-// }
 
