033334c77e5d3654fabd877927cad7dc50ebb728
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-17 17:13:34 +0100

    cpp08 ex02

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 16:22:03 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/05/17 16:40:35 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() : std::stack<T>() {}
+        MutantStack(const MutantStack &obj) : std::stack<T>(obj) {}
+        MutantStack &operator=(const MutantStack &obj)
+        {
+            if (this != &obj)
+                std::stack<T>::operator=(obj);
+            return *this;
+        }
+        ~MutantStack() {}
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+
+        iterator begin()
+        {
+            return this->c.begin();
+        }
+
+        iterator end()
+        {
+            return this->c.end();
+        }
+};
+
+#endif
