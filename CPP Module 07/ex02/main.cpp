9d1bf0d4164924dcc772a55cf254558df92b1ed7
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-18 18:11:04 +0100

    cpp07

---
+++ CPP Module 07/ex02/main.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 15:22:55 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/05/15 15:24:59 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Array.hpp"
+
+int main()
+{
+    std::cout << "------- integer array:" << std::endl;
+    try {
+        Array<int> a;
+        std::cout << "Size of a: " << a.size() << std::endl;
+
+        // ----Array of 5 integers -----
+        Array<int> b(5);
+        for (unsigned int i = 0; i < b.size(); ++i)
+            b[i] = i * 10;
+
+        // --- Copy constructor -----
+        Array<int> c(b);
+        c[2] = 4444;
+
+        // ---- Assignment operator -----
+        Array<int> d;
+        d = c;
+
+        // Print array
+        std::cout << "Array b: ";
+        for (unsigned int i = 0; i < b.size(); ++i)
+            std::cout << b[i] << " ";
+        std::cout << "\nArray c: ";
+        for (unsigned int i = 0; i < c.size(); ++i)
+            std::cout << c[i] << " ";
+        std::cout << "\nArray d: ";
+        for (unsigned int i = 0; i < d.size(); ++i)
+            std::cout << d[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << "Trying out-of-bounds access...\n";
+        std::cout << b[8];
+    }
+    catch (std::exception &e) {
+        std::cerr << "find exception: " << e.what() << std::endl;
+    }
+    
+std::cout << "------- char array:" << std::endl;
+    try {
+        Array<char> a;
+        std::cout << "Size of a: " << a.size() << std::endl;
+
+        Array<char> b(5);
+        for (unsigned int i = 0; i < b.size(); ++i)
+            b[i] = i + 65;
+            
+        Array<char> c(b);
+        c[2] = 'Z';
+
+        Array<char> d;
+        d = c;
+
+        std::cout << "Array b: ";
+        for (unsigned int i = 0; i < b.size(); ++i)
+            std::cout << b[i] << " ";
+        std::cout << "\nArray c: ";
+        for (unsigned int i = 0; i < c.size(); ++i)
+            std::cout << c[i] << " ";
+        std::cout << "\nArray d: ";
+        for (unsigned int i = 0; i < d.size(); ++i)
+            std::cout << d[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << "Trying out-of-bounds access...\n";
+        std::cout << b[8];
+    }
+    catch (std::exception &e) {
+        std::cerr << "find exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
