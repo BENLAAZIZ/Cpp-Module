2df80ac9481bfa8c97022f05883bd8e1beaadc8c
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-19 12:52:13 +0100

    cpp08 update  ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 18:16:53 by hben-laz          #+#    #+#             */
-/*   Updated: 2025/05/16 19:47:43 by hben-laz         ###   ########.fr       */
+/*   Updated: 2025/05/19 12:51:46 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 #define EASYFIND_HPP
 
 # include <iostream>
+# include <algorithm>
+#include <stdexcept>
 
 template <typename T>
-typename T::iterator easyfind(T &cont, int value)
+typename T::iterator easyfind(T &ct, int value)
 {
-    typename T::iterator it = std::find(cont.begin(), cont.end(), value);
-    if (it == cont.end()) {
+    typename T::iterator it = std::find(ct.begin(), ct.end(), value);
+    if (it == ct.end())
         throw std::runtime_error("nothing found");
-    }
     return it;
 }
 
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,24 +6,18 @@
 /*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 18:16:43 by hben-laz          #+#    #+#             */
-/*   Updated: 2025/05/16 19:51:51 by hben-laz         ###   ########.fr       */
+/*   Updated: 2025/05/19 12:51:27 by hben-laz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-#include <set>
 #include <vector>
-#include <map>
-#include <utility>
 #include <list>
-
+#include <set>
 
 int main() {
     std::vector<int> numbers;
     int nbr = 2;
-    // for (int i = 0; i < 5; ++i) {
-    //     numbers.push_back(i);
-    // }
 
     numbers.push_back(4);
     numbers.push_back(40);
@@ -34,7 +28,7 @@ int main() {
     try {
         std::vector<int>::iterator it = easyfind(numbers, nbr);
         std::cout << "Value found: " << *it << std::endl;
-    } catch (const std::runtime_error &e) {
+    } catch (std::exception &e) {
         std::cerr << "exception : " << e.what() << std::endl;
     }
 
@@ -44,13 +38,28 @@ int main() {
     ch.insert(ch.begin(), 'c');
     ch.insert(ch.begin(), 'v');
 
-    char c = 'p';
+    char c_value = 'p';
   
     try {
-        std::list<char>::iterator it = easyfind(ch, c);
+        std::list<char>::iterator it = easyfind(ch, c_value);
         std::cout << "Value found: " << *it << std::endl;
-    } catch (const std::runtime_error &e) {
+    } catch (std::exception &e) {
         std::cerr << "exception : " << e.what() << std::endl;
     }
+
+    std::set<int> s;
+    s.insert(1);
+    s.insert(-50);
+    s.insert(10);
+    s.insert(4);
+
+    int s_value = 10;
+    try {
+        std::set<int>::iterator it = easyfind(s, s_value);
+        std::cout << "Value found: " << *it << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << "exception : " << e.what() << std::endl;
+    }
+    
     return 0;
 }
