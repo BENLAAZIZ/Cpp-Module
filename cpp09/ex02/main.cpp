69e20f3052dc4875b472570d101dcd91585608f1
Hamza Ben laziz <benlaaziz.hamza@gmail.com>
2025-05-22 17:52:59 +0100

    ex02

---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hben-laz <hben-laz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 19:07:55 by hben-laz          #+#    #+#             */
+/*   Updated: 2025/05/22 11:58:30 by hben-laz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av)
+{
+	BitcoinExchange bit;
+	try
+	{
+		if (ac != 2)
+			throw std::runtime_error("Error: could not open file.");
+		bit.process_database();
+		bit.process_file(av[1]);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+}
